8d6d23b6024ebf1381efbf77f6a37b914d550a4e
BaelfireNightshd <25194649+BaelfireNightshd@users.noreply.github.com>
2020-07-08 16:03:32 -0700

    Initial Commit

---
+++ GenesisEmu/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GenesisEmu
+//
+//  Created by Tad Parrish on 7/8/20.
+//  Copyright © 2020 Tad Parrish. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
